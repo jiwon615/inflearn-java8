76322a7c0fe8aa4416820e7025a8e5e09d84f466
정지원 <jiwon@jeongjiwon-ui-Macmini.local>
2021-08-01 21:07:11 +0900

    feat: 자바에서 제공하는 함수형 인터페이스

--- src/main/java/me/whiteship/java/Foo.java
+++ src/main/java/me/whiteship/java/Foo.java
@@ -1,17 +1,34 @@
 package me.whiteship.java;
 
+import java.util.function.*;
+
 public class Foo {
 
     public static void main(String[] args) {
-        // 익명 내부 클래스
-        RunSomething runSomething = new RunSomething() {
+        Plus10 plus = new Plus10();
+        System.out.println("plus10 = " + plus.apply(1));
 
-            @Override
-            public void doIt() {
-                System.out.println("Hello");
-                System.out.println("World");
-            }
-        };
-        runSomething.doIt();
+        Function<Integer, Integer> plus10 = (i) -> i + 10; // Function -> UnaryOperator 타입으로 변경 가능
+        Function<Integer, Integer> multiply2 = (i) -> i * 2;
+
+        System.out.println("==Function==");
+        System.out.println(plus10.compose(multiply2).apply(2)); // compose 먼저한 결과값을 plus10에 대입
+        System.out.println(plus10.andThen(multiply2).apply(2)); // plus10 결과앖을 multiply에 대입
+
+        UnaryOperator<Integer> uPlus10 = (i) -> i + 10;
+        UnaryOperator<Integer> uMultiply2 = (i) -> i * 2;
+
+        System.out.println("==UnaryOperator==");
+        System.out.println(uPlus10.compose(uMultiply2).apply(2)); // compose 먼저한 결과값을 plus10에 대입
+        System.out.println(uPlus10.andThen(uMultiply2).apply(2)); // plus10 결과앖을 multiply에 대입
+
+        Consumer<Integer> printT = (i) -> System.out.println("Consumer<T> : " + i);
+        printT.accept(10);
+
+        Supplier<Integer> get10 = () -> 10;
+        System.out.println("Supplier<T> : " + get10.get());
+
+        Predicate<String> startsWithJiwon = (s) -> s.startsWith("jiwon");
+        Predicate<Integer> isEven = (i) -> i%2 == 0;
     }
 }
