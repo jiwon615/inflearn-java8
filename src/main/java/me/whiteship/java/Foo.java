7b5c2bc91aea91a7556ff125b475c6111ff71bf4
정지원 <seg615@naver.com>
2021-08-02 19:57:40 +0900

    feat: lamda expression

--- src/main/java/me/whiteship/java/Foo.java
+++ src/main/java/me/whiteship/java/Foo.java
@@ -32,5 +32,36 @@ public class Foo {
 
         Predicate<String> startsWithJiwon = (s) -> s.startsWith("jiwon");
         Predicate<Integer> isEven = (i) -> i%2 == 0;
+
+        BinaryOperator<Integer> sum = (a, b) -> a+ b;
+
+        Foo foo = new Foo();
+        foo.run();
+    }
+
+    private void run() {
+        int baseNumber = 10;  // effective final 변수: final 이란 키워드 없이도 사실상 final 변수 역할로서, 아래에서 참조 가능
+
+        // 로컬 클래스
+        class LocalClass {
+            void printBaseNumber() {
+                int baseNumber = 11; // 왼쪽 변수가 위에 선언된 동일이름의 변수를 가린 것
+                System.out.println(baseNumber);  // 11
+            }
+        }
+
+        // 익명 클래스
+        Consumer<Integer> integerConsumer = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer baseNumber) { // 여기서 baseNumber는 위에 선언된 baseNumber참조 안함.
+                System.out.println(baseNumber);
+            }
+        };
+
+        // 람다
+        IntConsumer printInt = (i) -> {
+            System.out.println(i + baseNumber);
+        };
+        printInt.accept(10);
     }
 }
