b1c39effe44f36aedd8c927fa168bb8b2d4a5c05
정지원 <seg615@naver.com>
2021-08-05 19:06:24 +0900

    feat: stream api

--- src/main/java/me/whiteship/java/App.java
+++ src/main/java/me/whiteship/java/App.java
@@ -1,7 +1,10 @@
 package me.whiteship.java;
 
+import me.whiteship.java.stream.OnlineClass;
+
 import java.util.*;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
@@ -33,66 +36,130 @@ public class App {
 //        Greeting lina = jiwonGreeting.apply("lina");
 //        System.out.println(lina.getName());
 
-        List<String> name = new ArrayList<>();
-        name.add("jiwon");
-        name.add("erica");
-        name.add("lina");
-        name.add("lucy");
-        name.add("lucy2");
-        // foreach
-        name.forEach( m -> {
-            System.out.println(m);
-        });
-        name.forEach(System.out::println);
+//        List<String> name = new ArrayList<>();
+//        name.add("jiwon");
+//        name.add("erica");
+//        name.add("lina");
+//        name.add("lucy");
+//        name.add("lucy2");
+//        // foreach
+//        name.forEach( m -> {
+//            System.out.println(m);
+//        });
+//        name.forEach(System.out::println);
 
         /**
          * 자바 8 API의 기본 메소드와 스태틱 메소드
          */
         // spliterator
-        Spliterator<String> spliterator = name.spliterator();
-        Spliterator<String> spliterator1 = spliterator.trySplit();
-        while (spliterator.tryAdvance(System.out::println));
-        System.out.println("=====");
-        while (spliterator1.tryAdvance(System.out::println));
-
-        // stream()
-        List<String> result = name.stream().map(String::toUpperCase)
-                .filter(m -> m.startsWith("L"))
-                .collect(Collectors.toList());
-        System.out.println("c = " + result);
-
-        // comparator()
-        System.out.println("==comparator()==");
-        Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
-        name.sort(compareToIgnoreCase.reversed());  // reverse()추가하면 역순으로 sort
-        name.forEach(System.out::println);
-
-        // removeIf()
-        System.out.println("==removeIf()==");
-        name.removeIf(m -> m.startsWith("l"));
-        name.forEach(System.out::println);
-        System.out.println("");
+//        Spliterator<String> spliterator = name.spliterator();
+//        Spliterator<String> spliterator1 = spliterator.trySplit();
+//        while (spliterator.tryAdvance(System.out::println));
+//        System.out.println("=====");
+//        while (spliterator1.tryAdvance(System.out::println));
+//
+//        // stream()
+//        List<String> result = name.stream().map(String::toUpperCase)
+//                .filter(m -> m.startsWith("L"))
+//                .collect(Collectors.toList());
+//        System.out.println("c = " + result);
+//
+//        // comparator()
+//        System.out.println("==comparator()==");
+//        Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
+//        name.sort(compareToIgnoreCase.reversed());  // reverse()추가하면 역순으로 sort
+//        name.forEach(System.out::println);
+//
+//        // removeIf()
+//        System.out.println("==removeIf()==");
+//        name.removeIf(m -> m.startsWith("l"));
+//        name.forEach(System.out::println);
+//        System.out.println("");
 
         /**
          * Stream
          */
-        List<String> streamEx = new ArrayList<>();
-        streamEx.add("lina");
-        streamEx.add("erica");
-        streamEx.add("jiwon");
-        streamEx.add("yuki");
+//        List<String> streamEx = new ArrayList<>();
+//        streamEx.add("lina");
+//        streamEx.add("erica");
+//        streamEx.add("jiwon");
+//        streamEx.add("yuki");
+//
+//        // 중계 Operator 예시 (ex. stream())
+//        Stream<String> stringStream = streamEx.stream().map(m -> {
+//            System.out.println("m = " + m);
+//            return m.toUpperCase();
+//        });
+//
+//        // 종료 Operator 예시 (ex. collect())
+//        List<String> collect = streamEx.stream().map(s -> {
+//            System.out.println("s = " + s);
+//            return s.toUpperCase();
+//        }).collect(Collectors.toList());
+//        collect.forEach(System.out::println);
 
-        // 중계 Operator 예시 (ex. stream())
-        Stream<String> stringStream = streamEx.stream().map(m -> {
-            System.out.println("m = " + m);
-            return m.toUpperCase();
-        });
 
-        // 종료 Operator 예시 (ex. collect())
-        List<String> collect = streamEx.stream().map(s -> {
-            System.out.println("s = " + s);
-            return s.toUpperCase();
-        }).collect(Collectors.toList());
-        collect.forEach(System.out::println);
+        List<OnlineClass> springClasses = new ArrayList<>();
+        springClasses.add(new OnlineClass(1, "spring boot", true));
+        springClasses.add(new OnlineClass(2, "spring data jpa", true));
+        springClasses.add(new OnlineClass(3, "spring mvc", false));
+        springClasses.add(new OnlineClass(4, "spring core", false));
+        springClasses.add(new OnlineClass(5, "rest api development", false));
+
+        System.out.println("--spring으로 시작하는 수업--");
+        springClasses.stream()
+                .filter(m -> m.getTitle().startsWith("spring"))
+                .forEach(m -> System.out.println(m.getTitle()));
+
+        System.out.println("--close 되지 않은 수업--");
+        springClasses.stream()
+                .filter(Predicate.not(OnlineClass::isClosed))  // .filter(m -> !m.isClosed())
+                .forEach(m -> System.out.println(m.getTitle()));
+
+        System.out.println("--수업 이름만 모아서 스트림 만들기--");
+        springClasses.stream()
+                .map(OnlineClass::getTitle)
+               // .map(m -> m.getTitle())
+                .forEach(System.out::println);
+
+        List<OnlineClass> javaClasses = new ArrayList<>();
+        javaClasses.add(new OnlineClass(6, "The Java, Test", true));
+        javaClasses.add(new OnlineClass(7, "The Java, Code manipulation", true));
+        javaClasses.add(new OnlineClass(8, "The Java, 8 to 11", false));
+
+        List<List<OnlineClass>> linaEvents = new ArrayList<>();
+        linaEvents.add(springClasses);
+        linaEvents.add(javaClasses);
+
+        System.out.println("--두 수업 목록에 들어있는 모든 수업 아이디 출력--");
+        linaEvents.stream()
+                .flatMap(Collection::stream)
+                .forEach(m -> System.out.println(m.getId()));
+
+        System.out.println("--10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10개 까지만 출력--");
+        Stream.iterate(10, i -> i+1)
+                .skip(10)
+                .limit(10)
+                .forEach(System.out::println);
+
+        System.out.println("--자바 수업 중에 Test가 들어있는 수업이 있는지 여부 확인--");
+        boolean test = javaClasses.stream()
+                .anyMatch(m -> m.getTitle().contains("Test"));
+        System.out.println(test);
+
+        System.out.println("-- 스프링 수업 중에 제목에 spring이 들어간 것만 모아서 List로 만들기 --");
+        List<String> result = springClasses.stream()
+                .filter(m -> m.getTitle().contains("spring"))
+                .map(OnlineClass::getTitle)
+                .collect(Collectors.toList());
+        result.forEach(System.out::println);
+
+        List<String> result2 = springClasses.stream()  // 위에것돠 filter, map 순서만 바꾼 동일 결과 코드
+                .map(OnlineClass::getTitle)
+                .filter(s -> s.contains("spring"))
+                .collect(Collectors.toList());
+        result2.forEach(System.out::println);
+
+
     }
 }
