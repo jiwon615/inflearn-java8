7a269a314535c3fd6b06564585eff70515ca2bfb
정지원 <seg615@naver.com>
2021-08-09 20:14:59 +0900

    modify: add coments

--- src/main/java/me/whiteship/java/DateTimeMain.java
+++ src/main/java/me/whiteship/java/DateTimeMain.java
@@ -14,14 +14,14 @@ public class DateTimeMain {
         ZoneId zone = ZoneId.systemDefault();
         System.out.println(zone);  // Asia/Seoul
         ZonedDateTime zonedDateTime = instant.atZone(zone);
-        System.out.println("zonedDateTime = " + zonedDateTime);
+        System.out.println("zonedDateTime = " + zonedDateTime);  // 2021-08-09T19:46:03.085796+09:00[Asia/Seoul]
 
         // 인류용
         LocalDateTime now = LocalDateTime.now();
-        System.out.println("now = " + now);
+        System.out.println("now = " + now);  // 2021-08-09T19:46:03.146227
         LocalDateTime birthDay = LocalDateTime.of(1994, Month.JUNE, 15, 0, 0, 0); // 특정 시간대 해당하는 날짜 만들기
         ZonedDateTime nowInKorea = ZonedDateTime.now(ZoneId.of("Asia/Seoul")); // 특정 존의 날짜 시간
-        System.out.println(nowInKorea);
+        System.out.println(nowInKorea);  // 2021-08-09T19:46:03.146619+09:00[Asia/Seoul]
 
         // 기간을 표현하는 방법 (email 변경)
         LocalDate today = LocalDate.now();
